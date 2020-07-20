16db68ef185e8783761f28e01e82e7e422d5af3a
Romario Gustave <romagustave10@gmail.com>
2020-07-20 00:00:07 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 
-
+class User < ActiveRecord::Base
+  validates_presence_of :name, :balance, :password
+  def change 
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.float :balance
+    end 
+  end 
+end 
