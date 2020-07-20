d09b782d5ef2d657b7ca526f1c41ede50377287a
Romario Gustave <romagustave10@gmail.com>
2020-07-20 00:16:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,17 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-   get '/' do
+  get '/' do
     erb :index
   end
 
   post '/login' do
-    user = User.find_by(username: params["username"], password: params["password"])
-    if user
-      session[:user_id] = user.id
-      redirect to '/account'
-    else
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user.nil?
       erb :error
+    else
+      session[:user_id] = @user.id
+      redirect '/account'
     end
   end
 
@@ -26,9 +26,8 @@ class ApplicationController < Sinatra::Base
 
   get '/logout' do
     session.clear
-    redirect to '/'
+    redirect '/'
   end
 
 
 end
-
